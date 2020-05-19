fc6bd30ded46858bf3137c41820eb7d4094b3b90
KT12252325 <65146297+KT12252325@users.noreply.github.com>
2020-05-19 16:33:46 +0900

    Add files via upload

---
+++ ５月15日takumi/ELeftCheck.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ELeftCheck : MonoBehaviour
+{
+    private string PlayerTag = "Player";
+    public bool isleft = false;
+    private bool isleftEnter, isleftStay, isleftExit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public bool Isleft()
+    {
+        if (isleftEnter || isleftStay)
+        {
+            isleft = true;
+        }
+        else if (isleftExit)
+        {
+            isleft = false;
+        }
+
+        isleftEnter = false;
+        isleftStay = false;
+        isleftExit = false;
+        return isleft;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == PlayerTag)
+        {
+            isleftEnter = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == PlayerTag)
+        {
+            isleftStay = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == PlayerTag)
+        {
+            isleftExit = true;
+        }
+    }
+}

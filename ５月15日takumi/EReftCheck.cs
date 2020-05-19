45b073615563a1aff352751c7da73cb0bff2a6c9
KT12252325 <65146297+KT12252325@users.noreply.github.com>
2020-05-19 16:54:30 +0900

    Add files via upload

---
+++ ５月15日takumi/EReftCheck.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ERightCheck : MonoBehaviour
+{
+    private string PlayerTag = "Player";
+    public bool isright = false;
+    private bool isrightEnter, isrightStay, isrightExit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public bool Isright()
+    {
+        if (isrightEnter || isrightStay)
+        {
+            isright = true;
+        }
+        else if (isrightExit)
+        {
+            isright = false;
+        }
+
+        isrightEnter = false;
+        isrightStay = false;
+        isrightExit = false;
+        return isright;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == PlayerTag)
+        {
+            isrightEnter = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == PlayerTag)
+        {
+            isrightStay = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == PlayerTag)
+        {
+            isrightExit = true;
+        }
+    }
+}

029c70cf5bab1164f01687340ba46555e6c39add
KT12252325 <65146297+KT12252325@users.noreply.github.com>
2020-06-02 10:50:38 +0900

    Add files via upload

---
+++ ５月15日takumi/BossAtack.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAtack : MonoBehaviour
+{
+    //プレイヤーのオブジェクト
+    public GameObject player;
+    //弾のプレハブオブジェクト
+    public EBullet enemyBullet;
+    //プレイヤーの方を向く
+    public ELeftCheck left;
+    public ERightCheck right;
+    // 弾の移動の速さ
+    public float EshotSpeed;
+    // 複数の弾を発射する時の角度
+    public float EshotAngleRange;
+    // 弾の発射タイミングを管理するタイマー
+    public float EshotTimer;
+    // 弾の発射数
+    public int EshotCount;
+    // 弾の発射間隔（秒）
+    public float EshotInterval;
+
+    public float angle = 0;
+
+    private bool isleft = false;
+    private bool isright = false;
+
+  
+    //画面に移ったら行動開始
+    private SpriteRenderer sr = null;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        isleft = left.Isleft();
+        isright = right.Isright();
+        if (sr.isVisible)
+        {
+            // 弾の発射タイミングを管理するタイマーを更新する
+            EshotTimer += Time.deltaTime;
+
+            // まだ弾の発射タイミングではない場合は、ここで処理を終える
+            if (EshotTimer < EshotInterval) return;
+
+            // 弾の発射タイミングを管理するタイマーをリセットする
+            EshotTimer = 0;
+
+            if (isleft)
+            {
+                //EnemyのlocalScaleを変数に格納
+                Vector2 temp = gameObject.transform.localScale;
+                //localScale.xに1をかける
+                temp.x *= 1;
+                //結果を戻す
+                gameObject.transform.localScale = temp;
+                // 弾を発射する
+                ShootNWay(angle, EshotAngleRange, EshotSpeed, EshotCount);
+            }
+            if (isright)
+            {
+                //EnemyのlocalScaleを変数に格納
+                Vector2 temp = gameObject.transform.localScale;
+                //localScale.xに-1をかける
+                temp.x *= -1;
+                //結果を戻す
+                gameObject.transform.localScale = temp;
+                // 弾を発射する
+                ShootNWay(angle, EshotAngleRange, EshotSpeed, EshotCount);
+            }
+        }
+    }
+
+  
+    
+    // 弾を発射する関数
+    private void ShootNWay(
+        float angleBase, float angleRange, float speed, int count)
+    {
+        var pos = transform.localPosition; // プレイヤーの位置
+        var rot = transform.localRotation; // プレイヤーの向き
+
+        // 弾を複数発射する場合
+        if (1 < count)
+        {
+            // 発射する回数分ループする
+            for (int i = 0; i < count; ++i)
+            {
+                // 弾の発射角度を計算する
+                var angle = angleBase +
+                    angleRange * ((float)i / (count - 1) - 0.5f);
+
+                // 発射する弾を生成する
+                var ebullet = Instantiate(enemyBullet, pos, rot);
+
+                // 弾を発射する方向と速さを設定する
+                ebullet.Init(angle, speed);
+            }
+        }
+        // 弾を 1 つだけ発射する場合
+        else if (count == 1)
+        {
+            // 発射する弾を生成する
+            var ebullet = Instantiate(enemyBullet, pos, rot);
+
+            // 弾を発射する方向と速さを設定する
+            ebullet.Init(angleBase, speed);
+        }
+
+
+    }
+}
---
+++ ５月15日takumi/BossControl.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossControl : MonoBehaviour
+{
+    //Rigidbodey2DコンポーネントにアクセスするRigidbody型変数rb2dを宣言
+    Rigidbody2D rb2d;
+    //プレイヤーの方を向く
+    public ELeftCheck left;
+    public ERightCheck right;
+    //体力
+    public int hp = 50;
+    //移動速度
+    public float speed = 2.0f;
+    //ジャンプしているかいないか
+    bool jump = false;
+    //ジャンプ力
+    float flap = 200f;
+    
+    //画面に移ったら行動開始
+    private SpriteRenderer sr = null;
+
+    private bool isleft = false;
+    private bool isright = false;
+    // 爆発効果音
+    public AudioClip explosionSE;
+    //ジャンプ時の音
+    public AudioClip sound1;
+    AudioSource audio;
+    void Start()
+    {
+        //Rigidbody2D関数にアクセスして実体化
+        rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+        if (sr.isVisible)
+        {
+            rb2d.velocity = new Vector2(-transform.localScale.x * speed, rb2d.velocity.y);
+            if (jump == false)
+            {
+                rb2d.AddForce(Vector2.up * flap);
+                jump = true;
+                if(jump==true)
+                {
+                    audio.Play();
+                }
+            }
+            if (isleft)
+            {
+                speed = speed * -1;
+            }
+            if (isright)
+            {
+                speed = speed * -1;
+            }
+        }
+
+        //hpが0になった時死ぬ
+        if (hp <= 0)
+        {
+            // オーディオを再生
+            AudioSource.PlayClipAtPoint(explosionSE, transform.position);
+            Destroy(gameObject);
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Wallタグのついたオブジェクトと衝突したら
+        if (col.gameObject.tag == "Wall")
+        {
+            //EnemyのlocalScaleを変数に格納
+            Vector2 temp = gameObject.transform.localScale;
+            //localScale.xに-1をかける
+            temp.x *= -1;
+            //結果を戻す
+            gameObject.transform.localScale = temp;
+        }
+        //Wallタグのついたオブジェクトと衝突したら
+        if (col.gameObject.tag == "Enemy")
+        {
+            //EnemyのlocalScaleを変数に格納
+            Vector2 temp = gameObject.transform.localScale;
+            //localScale.xに-1をかける
+            temp.x *= -1;
+            //結果を戻す
+            gameObject.transform.localScale = temp;
+        }
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            jump = false;
+
+        }
+       
+    }
+
+   
+}

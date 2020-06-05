2eefba9829a71f5033f6b2c169357684e3caa3d9
白垣一馬 <1816219@s.asojuku.ac.jp>
2020-06-05 16:39:57 +0900

    HPの調整、無敵時間の追加、ポーションに触れた時の処理を追加

--- kazuma/Scripts/PlayerHealth.cs
+++ kazuma/Scripts/PlayerHealth.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     //プライベート変数
     private Slider playerHPSlider;
+    private SpriteRenderer renderer;
 
-　　//パブリック変数
+    //パブリック変数
     //エフェクト
     public GameObject effectPrefab;
     //体力
@@ -17,38 +19,68 @@ public class PlayerHealth : MonoBehaviour
     public GameObject[] playerIcons;
     //プレイヤーがやられた回数のカウンター
     public int deathCount = 0;
+    public bool on_damage = false;
+
+    public GameObject startPoint;
+
+    public AudioClip Revival_PotionSE;
+    public AudioClip HP_PotionSE;
+    public AudioSource audioSource;
+
+
 
     private void Start()
     {
         playerHPSlider = GameObject.Find("PlayerHPSlider").GetComponent<Slider>();
         playerHPSlider.maxValue = playerHP;
         playerHPSlider.value = playerHP;
+        //点滅処理のために呼ぶ
+        renderer = gameObject.GetComponent<SpriteRenderer>();
+
+        //AudioSource関数にアクセスして実体化
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Revival_PotionSE;
+        audioSource.clip = HP_PotionSE;
+    }
+
+    void Update()
+    {
+        //ダメージフラグがtrueだったら点滅
+        if(on_damage)
+        {
+            float level = Mathf.Abs(Mathf.Sin(Time.time * 10));
+            renderer.color = new Color(1f, 1f, 1f, level);
+        }
+
+        if (playerHP <= 0)
+        {
+            //HPが0になったらカウント+1
+            deathCount += 1;
+            if (deathCount > 3)
+            {
+                SceneManager.LoadScene("GameOver");
+            }
+            UpdatePlayerIcons();
+
+            GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
+            Destroy(effect, 1.0f);
+            //プレイヤーを非アクティブにする
+            this.gameObject.SetActive(false);
+            //リトライの命令ブロックを1秒後に呼び出す  
+            Invoke("Retry", 1.0f);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        //敵とぶつかったかつ、ダメージフラグがfalseだったら
+        if (!on_damage && collision.gameObject.CompareTag("Enemy"))
         {
             playerHP -= 1;
 
             playerHPSlider.value = playerHP;
 
-            if (playerHP == 0)
-            {
-                //HPが0になったらカウント+1
-                deathCount += 1;
-
-                UpdatePlayerIcons();
-
-                GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
-                //1秒後にエフェクトを削除
-                Destroy(effect, 1.0f);
-                //プレイヤーを非アクティブにする
-                this.gameObject.SetActive(false);
-
-                //リトライを1秒後に呼び出す  
-                Invoke("Retry", 1.0f);
-            }
+            OnDamageEffect();
         }
     }
 
@@ -61,30 +93,77 @@ public class PlayerHealth : MonoBehaviour
 
             playerHPSlider.value = playerHP;
 
-            if (playerHP == 0)
+            OnDamageEffect();
+        }
+
+        //入手したらHP回復
+        if(collider.gameObject.CompareTag("HP_Potion"))
+        {
+            //SEを鳴らす
+            audioSource.PlayOneShot(HP_PotionSE);
+            playerHP += 3;
+
+            playerHPSlider.value += 3;
+
+            if(playerHP > 5)
             {
-                //HPが0になったらカウント+1
-                deathCount += 1;
+                playerHP = 5;
+            }
+        }
 
-                UpdatePlayerIcons();
-
-                GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
-                Destroy(effect, 1.0f);
-                //プレイヤーを非アクティブにする
-                this.gameObject.SetActive(false);
-
-                //リトライの命令ブロックを1秒後に呼び出す  
-                Invoke("Retry", 1.0f);
+        if (collider.gameObject.CompareTag("Revival_Potion"))
+        {
+            //SEを鳴らす
+            audioSource.PlayOneShot(Revival_PotionSE);
+            deathCount -= 1;
+            UpdatePlayerIcons();
+            if (deathCount < 0)
+            {
+                deathCount = 0;
             }
         }
     }
 
-    //プレイヤーの残機数を更新する
+    //ダメージを受けた際の動き
+    void OnDamageEffect()
+    {
+        //ダメージフラグをON
+        on_damage = true;
+        
+        //プレイヤーの位置を後ろに飛ばす
+        float s = 10f * Time.deltaTime;
+        transform.Translate(Vector3.up * s);
+
+        //プレイヤーのlocalScaleでどちらを向いているのかを判定
+        if(transform.localScale.x >= 0)
+        {
+            transform.Translate(Vector3.left * s);
+        }
+        else
+        {
+            transform.Translate(Vector3.right * s);
+        }
+
+        // コルーチン開始
+        StartCoroutine("WaitForIt");
+    }
+
+    IEnumerator WaitForIt()
+    {
+        // 1秒間処理を止める
+        yield return new WaitForSeconds(1);
+
+        // １秒後ダメージフラグをfalseにして点滅を戻す
+        on_damage = false;
+        renderer.color = new Color(1f, 1f, 1f, 1f);
+    }
+
+    //プレイヤーの残機数を表示する命令ブロック
     void UpdatePlayerIcons()
     {
         for(int i = 0;i < playerIcons.Length;i++)
         {
-            if(deathCount <= i)
+            if (deathCount <= i)
             {
                 playerIcons[i].SetActive(true);
             }
@@ -99,7 +178,15 @@ public class PlayerHealth : MonoBehaviour
     void Retry()
     {
         this.gameObject.SetActive(true);
-        playerHP = 10;
+        playerHP = 5;
         playerHPSlider.value = playerHP;
+        on_damage = false;
+        renderer.color = new Color(1f, 1f, 1f, 1f);
+
+        gameObject.transform.position = startPoint.transform.position;
+
+        //// 現在のシーンを再読み込み
+        //int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        //SceneManager.LoadScene(sceneIndex);
     }
 }

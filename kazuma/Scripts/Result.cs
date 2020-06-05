e0e634f74a31e50bae77253af8b32a93ec718482
白垣一馬 <1816219@s.asojuku.ac.jp>
2020-06-05 16:43:26 +0900

    リザルト画面のスコア用のスクリプト

---
+++ kazuma/Scripts/Result.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Result : MonoBehaviour
+{
+    // ハイスコアを表示するUIの取得用
+    public Text scoreText;
+
+    int resultScore;
+
+    void Start()
+    {
+        resultScore = Score.getScore();
+    }
+
+    void Update()
+    {
+        scoreText.text = resultScore.ToString();
+    }
+}
